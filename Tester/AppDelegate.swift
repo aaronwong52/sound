fe37be613047a61fde0972bb2ddc966a6b5de481
Aaron Wong <aaron.wong@tufts.edu>
2017-11-28 14:43:48 -0500

    Initial Commit

---
+++ Tester/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tester
+//
+//  Created by Aaron Wong on 11/28/17.
+//  Copyright © 2017 Aaron Wong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tester/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tester
+//
+//  Created by Aaron Wong on 11/28/17.
+//  Copyright © 2017 Aaron Wong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
